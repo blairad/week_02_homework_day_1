a342be7ec08f63f5bc6ea14a3309f3e5c5415609
Andrew Blair <blair.d.andrew@gmail.com>
2019-04-08 16:50:46 +0100

    regen_4

--- spec/spec.sport_team.rb
+++ spec/spec.sport_team.rb
@@ -26,8 +26,8 @@ class TestTeam < MiniTest::Test
 
   def test_add_new_player
     team = Team.new('Spurs',['regen_1', 'regen_2', 'regen_3'], 'Sven')
-    team = team.add_new_player('regen_4')
-    assert_equal(4, team.players.new_signing)
+    team.add_new_player('regen_4')
+    assert_equal(4, team.players.count)
 
   end
 
